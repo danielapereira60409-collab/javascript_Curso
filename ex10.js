faa9ea2d43de944df888662ba6a97a7f0dc83200
Daniela Pereira Costa <1058912@senacminas.edu.br>
2025-11-17 16:31:18 -0300

    Frase

--- ex10.js
+++ ex10.js
@@ -1,2 +1,10 @@
 //Meu documento inicial
+const prompt=require("prompt-sync")();
+let numeros=[]
 
+for(let i=0;i<10;i++){
+    let numero=Number(prompt(`Digite o ${i} numero`));
+    numeros[i] = numero;
+}
+console.log("\n Os números adicionados foram:");
+console.log(numeros);
