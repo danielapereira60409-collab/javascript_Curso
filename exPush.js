162dc0988b34aa6d15375d96e6243c3e05883a01
Daniela Pereira Costa <1058912@senacminas.edu.br>
2025-11-18 15:26:33 -0300

    exemplo cosme

---
+++ exPush.js
@@ -0,0 +1,9 @@
+const prompt=require("prompt-sync")({sigint:true});
+
+let jogos = ["Minecraft", "Fortnite"];
+
+jogos.push("Roblox");
+console.log(jogos);
+
+jogos.push("among us", "FIFA");
+console.log(jogos);
