b174a3dd061788ff658163a6ed88fc3ebb1827ed
Daniela Pereira Costa <1058912@senacminas.edu.br>
2025-11-25 14:03:46 -0300

    av1

---
+++ av1.js
@@ -0,0 +1,11 @@
+const prompt=require("prompt-sync")({sigint:true});
+
+const alunos= [
+    {nome:"Ana", nota: 8},
+    {nome:"Pedro", nota: 5},
+    {nome:"Carla", nota: 9},
+    {nome:"Lucas", nota: 4}
+];
+
+const aprovados=alunos.filter(aluno=> aluno.nota>6);
+console.log(aprovados)
