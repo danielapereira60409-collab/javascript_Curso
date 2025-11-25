e1d22816fa67ea93914c03dfc4936fd7a927d729
Daniela Pereira Costa <1058912@senacminas.edu.br>
2025-11-25 14:12:54 -0300

    av2

---
+++ av2.js
@@ -0,0 +1,11 @@
+const prompt=require("prompt-sync")({sigint:true});
+ 
+const produtos= [
+    {id:1, nome:"Fone Bluetooth"},
+    {id:2, nome:"Mouse gamer"},
+    {id:3, nome:"Fone com fio"},
+    {id:4, nome:"Teclado Mecânico"}
+];
+
+const resultado= produtos.filter(produto=>produto.nome.includes("Fone"));
+console.log(resultado)
